09da4f083aae657fab7d659641561070cf372ad9
tsukumi <tsukumijima@users.noreply.github.com>
2023-03-06 06:09:45 +0900

    細かな修正

--- cmake/ElfInterp.cmake
+++ cmake/ElfInterp.cmake
@@ -4,9 +4,9 @@ if(OBJCOPY_EXECUTABLE)
 	set(ELF_INTERP_BIN ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeTmp/elf_interp)
 	set(ELF_INTERP_SRC ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeTmp/elf_interp.c)
 	set(ELF_INTERP_DAT ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeTmp/elf_interp.dat)
-	
+
 	file(WRITE ${ELF_INTERP_SRC} "int main(int argc, char **argv) { return 0; }")
-	
+
 	execute_process(COMMAND ${CMAKE_C_COMPILER} ${CMAKE_C_FLAGS} -o ${ELF_INTERP_BIN} ${ELF_INTERP_SRC})
 	execute_process(COMMAND ${OBJCOPY_EXECUTABLE} -O binary --only-section=.interp ${ELF_INTERP_BIN} ${ELF_INTERP_DAT})
 
