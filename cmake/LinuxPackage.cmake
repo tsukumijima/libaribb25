c06870cb1d70c1c56ada7be57566b51960ec24ac
tsukumi <tsukumijima@users.noreply.github.com>
2023-03-05 22:31:06 +0000

    deb パッケージのファイル名を Debian の標準に合わせる

--- cmake/LinuxPackage.cmake
+++ cmake/LinuxPackage.cmake
@@ -10,5 +10,9 @@ SET(CPACK_PACKAGE_CONTACT "tsukumijima@users.noreply.github.com")
 SET(CPACK_DEBIAN_PACKAGE_MAINTAINER "tsukumi")
 set(CPACK_DEB_COMPONENT_INSTALL YES)
 
+# deb パッケージのファイル名を Debian の標準に合わせる
+# ref: https://stackoverflow.com/a/59202791/17124142
+set(CPACK_DEBIAN_FILE_NAME DEB-DEFAULT)
+
 # CPack は最後にインポートする
 include(CPack)
